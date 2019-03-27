c7f13a04fc902222e01e9ce516e29d0edfc783b6
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-03-27 17:30:50 -0300

    Commit inicial

---
+++ HangMan/HangMan/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random sorteio = new Random();
+            String[] linhas = File.ReadAllLines("palavras.txt");
+            // ElementAt(posicao) = pega o elemento na posicao dada
+            Char[] palavraOculta = linhas.ElementAt(sorteio.Next(0, linhas.Length)).ToCharArray();
+
+            Char[] palavra = new Char[palavraOculta.Length];
+
+            for (Byte i = 0; i < palavra.Length; i++)
+            {
+                palavra[i] = '_';
+            }
+
+            Char letra;
+            Byte letrasDaPalavra = 0;
+            Boolean notificaMudanca = false;
+            Byte erros = 0;
+
+            do
+            {
+                ImprimeJogo(palavra, erros);
+                Console.Write("Digite uma letra: ");
+                letra = (Console.ReadLine())[0];
+
+                for (Byte i = 0; i < palavraOculta.Length; i++)
+                {
+                    if (palavraOculta[i] == letra)
+                    {
+                        palavra[i] = letra;
+                        letrasDaPalavra++;
+                        notificaMudanca = true;
+                    }
+                }
+
+                if (!notificaMudanca)
+                {
+                    erros++;
+                }
+                else
+                {
+                    notificaMudanca = false;
+                }
+            } while (letrasDaPalavra < palavraOculta.Length && erros < 6);
+
+            ImprimeJogo(palavra, erros);
+
+            if (letrasDaPalavra < palavraOculta.Length)
+            {
+                // GAME OVER
+                Console.WriteLine();
+                Console.WriteLine("LOOOOOOSER!!!");
+                Console.WriteLine();
+                Console.Write("A palavra era: ");
+                Console.WriteLine(palavraOculta);
+            }
+            else
+            {
+                // CONGRATULATIONS
+                Console.WriteLine();
+                Console.WriteLine("PARABÉNS !!!");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair ...");
+            Console.ReadKey();
+        }
+
+        static void ImprimeJogo(Char[] palavra, Byte erros)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("### HANGMAN ###");
+            Console.WriteLine();
+            Console.WriteLine(" _______ ");
+            Console.WriteLine(" |     | ");
+
+            // IMPRIME CABECA
+            if (erros > 0)
+            {
+                Console.WriteLine(" |     O ");
+            }
+            else
+            {
+                Console.WriteLine(" |       ");
+            }
+            
+            // IMPRIME CORPO COM BRACOS
+            if (erros > 1)
+            {
+                if (erros > 2)
+                {
+                    if (erros > 3)
+                    {
+                        Console.WriteLine(@" |    /|\");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" |    /| ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(" |     | ");
+                }
+                Console.WriteLine(" |     | ");
+            }
+            else
+            {
+                Console.WriteLine(" |       ");
+                Console.WriteLine(" |       ");
+            }
+
+            // IMPRIME PERNAS
+            if (erros > 4)
+            {
+                if (erros > 5)
+                {
+                    Console.WriteLine(@" |    / \");
+                }
+                else
+                {
+                    Console.WriteLine(" |    /  ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" |       ");
+            }
+            Console.WriteLine(" |       ");
+
+            Console.WriteLine();
+            Console.WriteLine(palavra);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HangMan/HangMan/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("HangMan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HangMan")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("bd1f0d07-c750-4523-b7fb-37f4ef187e23")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
