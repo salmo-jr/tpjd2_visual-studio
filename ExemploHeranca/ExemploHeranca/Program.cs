eb32c07af5d8b72096edc3c9cd57fa4a8040e055
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-04-01 17:28:55 -0300

    Início do exemplo "ExemploHeranca"

---
+++ ExemploHeranca/ExemploHeranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sprite heroi = new Sprite();
+            Sprite inimigo;
+
+            heroi.Nome = "Batman";
+            heroi.PosicaoX = 100;
+            heroi.andaEsquerda();
+        }
+    }
+}
